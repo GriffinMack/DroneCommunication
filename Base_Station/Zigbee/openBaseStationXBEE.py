32d5890b2057bd1f96d1dbcf7667be4a8f3576be
GriffinMack <47880617+GriffinMack@users.noreply.github.com>
2020-08-29 01:26:25 -0700

    Combined flight control functionality, Added a script to find the serial port XBEE is attached to

--- Base_Station/Zigbee/openBaseStationXBEE.py
+++ Base_Station/Zigbee/openBaseStationXBEE.py
@@ -1,7 +1,34 @@
 from digi.xbee.devices import DigiMeshDevice
+import serial.tools.list_ports
+
+
+def findCorrectSerialPort():
+    portsList = serial.tools.list_ports.comports()
+    serialPorts = []
+    for port, desc, hwid in sorted(portsList):
+        if desc != "n/a":
+            serialPorts.append(port)
+
+    for port in serialPorts:
+        try:
+            device = DigiMeshDevice(port, 9600)
+            device.open()
+            correctPort = port
+        except Exception:
+            pass
+    return correctPort
+
 
 def openBaseStationXBEE():
-    device = DigiMeshDevice("/dev/tty.usbserial-0001", 9600)
+    correctSerialPort = findCorrectSerialPort()
+    device = DigiMeshDevice(correctSerialPort, 9600)
+    connected = False
+    while not connected:
+        try:
+            device.open()
+            connected = True
+        except Exception:
+            # TODO: Weird issue with double exception. Doesn't bother anything but the printout is really annoying
+            pass
 
-    device.open()
     return device
