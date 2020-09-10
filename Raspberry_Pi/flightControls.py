9695185953f7df2a04db50a6acfedb23d24653eb
GriffinMack <47880617+GriffinMack@users.noreply.github.com>
2020-09-10 00:20:32 -0700

    Changed 'coordinate' message, added some exceptions

--- Raspberry_Pi/flightControls.py
+++ Raspberry_Pi/flightControls.py
@@ -7,7 +7,7 @@ def decodeMessage(droneDevice, incomingMessage):
     flightControls = {
         "takeoff": takeoffDrone,
         "land": landDrone,
-        "move to coordinate: ": moveToCoordinates,
+        "move to coordinate": moveToCoordinates,
         "return to home without landing": homeLocationHover,
         "follow me": followBaseStation,
         "debug": getDroneSummary,
@@ -21,37 +21,43 @@ def decodeMessage(droneDevice, incomingMessage):
         "left": manuallyMoveDroneLeft,
         "right": manuallyMoveDroneRight,
     }
-    # TODO: This function call will not work with the move to coordinate: () message
-    return flightControls[incomingMessage](droneDevice.pixhawkDevice)
+    # Check for any additional info in the command (should be split by a :)
+    try:
+        incomingMessage, additionalInfo = incomingMessage.split(':')
+    except:
+        additionalInfo = None
+    return flightControls[incomingMessage](droneDevice.pixhawkDevice, additionalInfo)
 
 
-def getDroneCoordinates(pixhawkDevice):
+def getDroneCoordinates(pixhawkDevice, additionalInfo=None):
     pass
     # TODO: Go through these items and find ones that are actually helpful. Maybe leave all non-helpful items in and allow an optional "verbose" call
 
     # TODO: Take the most helpful items and send them back to the base station through the Zigbee (find an efficient way to do this. We don't want to send 20 different messages just for basic info. Maybe combine everything into one string and break it back up on the other end)
 
 
-def getDroneSummary(pixhawkDevice):
+def getDroneSummary(pixhawkDevice, additionalInfo=None):
     pass
     # TODO: Go through these items and find ones that are actually helpful. Maybe leave all non-helpful items in and allow an optional "verbose" call
 
     # TODO: Take the most helpful items and send them back to the base station through the Zigbee (find an efficient way to do this. We don't want to send 20 different messages just for basic info. Maybe combine everything into one string and break it back up on the other end)
 
 
-def takeoffDrone(pixhawkDevice):
+def takeoffDrone(pixhawkDevice, additionalInfo=None):
     async def run():
         print("Waiting for drone to have a global position estimate...")
         async for health in pixhawkDevice.pixhawkVehicle.telemetry.health():
             if health.is_global_position_ok:
                 print("Global position estimate ok")
                 break
+        try:
+            print("-- Arming")
+            await pixhawkDevice.pixhawkVehicle.action.arm()
 
-        print("-- Arming")
-        await pixhawkDevice.pixhawkVehicle.action.arm()
-
-        print("-- Taking off")
-        await pixhawkDevice.pixhawkVehicle.action.takeoff()
+            print("-- Taking off")
+            await pixhawkDevice.pixhawkVehicle.action.takeoff()
+        except Exception as e:
+            print(e)
 
         # TODO: check that the drone has reached the takeoff altitude
 
@@ -59,7 +65,7 @@ def takeoffDrone(pixhawkDevice):
     loop.run_until_complete(run())
 
 
-def landDrone(pixhawkDevice):
+def landDrone(pixhawkDevice, additionalInfo=None):
     async def run():
         print("Waiting for drone to have a global position estimate...")
         async for health in pixhawkDevice.pixhawkVehicle.telemetry.health():
@@ -68,7 +74,10 @@ def landDrone(pixhawkDevice):
                 break
 
         print("-- Landing")
-        await pixhawkDevice.pixhawkVehicle.action.land()
+        try:
+            await pixhawkDevice.pixhawkVehicle.action.land()
+        except Exception as e:
+            print(e)
 
         # TODO: check that the drone has reached the ground
 
@@ -76,7 +85,7 @@ def landDrone(pixhawkDevice):
     loop.run_until_complete(run())
 
 
-def moveToCoordinates(pixhawkDevice):
+def moveToCoordinates(pixhawkDevice, additionalInfo=None):
     async def run():
         # TODO: Get the user inputted coordinates from the XBEE message
         print("Waiting for drone to have a global position estimate...")
@@ -92,21 +101,23 @@ def moveToCoordinates(pixhawkDevice):
             longitude = terrain_info.longitude_deg
             break
 
+        # TODO: Check if the drone is actually in the air
+
         await asyncio.sleep(1)
         flying_alt = absolute_altitude + 20.0  # To fly drone 20m above the ground plane
 
         # goto_location() takes Absolute MSL altitude
         await pixhawkDevice.pixhawkVehicle.action.goto_location(
-            latitude + 0.002, longitude + 0.002, flying_alt, 0
+            latitude + 0.0002, longitude + 0.0002, flying_alt, 0
         )
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
 
 
-def homeLocationHover(pixhawkDevice):
+def homeLocationHover(pixhawkDevice, additionalInfo=None):
     async def run():
-        # TODO: Get the user inputted coordinates from the XBEE message
+        # TODO: Check if the drone is actually in the air
         print("Waiting for drone to have a global position estimate...")
         async for health in pixhawkDevice.pixhawkVehicle.telemetry.health():
             if health.is_global_position_ok:
@@ -132,46 +143,46 @@ def homeLocationHover(pixhawkDevice):
     loop.run_until_complete(run())
 
 
-def followBaseStation(pixhawkDevice):
+def followBaseStation(pixhawkDevice, additionalInfo=None):
     pass
 
 
 # TODO: All of these functions can utilize the ManualControl class of mavsdk
-def manuallyMoveDroneUp(pixhawkDevice):
+def manuallyMoveDroneUp(pixhawkDevice, additionalInfo=None):
     print("moving drone left")
     # Z +1
 
 
-def manuallyRotateDroneLeft(droneDevice):
+def manuallyRotateDroneLeft(droneDevice, additionalInfo=None):
     print("moving drone left")
     # r -1
 
 
-def manuallyRotateDroneRight(droneDevice):
+def manuallyRotateDroneRight(droneDevice, additionalInfo=None):
     print("moving drone right")
     # r +1
 
 
-def manuallyMoveDroneDown(droneDevice):
+def manuallyMoveDroneDown(droneDevice, additionalInfo=None):
     print("moving drone down")
     # Z -1
 
 
-def manuallyMoveDroneForward(droneDevice):
+def manuallyMoveDroneForward(droneDevice, additionalInfo=None):
     print("moving drone forward")
     # X +1
 
 
-def manuallyMoveDroneBackward(droneDevice):
+def manuallyMoveDroneBackward(droneDevice, additionalInfo=None):
     print("moving drone backward")
     # X -1
 
 
-def manuallyMoveDroneLeft(droneDevice):
+def manuallyMoveDroneLeft(droneDevice, additionalInfo=None):
     print("moving drone left")
     # Y -1
 
 
-def manuallyMoveDroneRight(droneDevice):
+def manuallyMoveDroneRight(droneDevice, additionalInfo=None):
     print("moving drone right")
     # Y +1
